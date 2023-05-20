2c9bfe2d5b7b013e41e3548b2f31814272d48b57
Danish Zaman Khan <danishzamankhan1996@gmail.com>
2023-05-20 15:06:12 +0530

    Create the new app 'Simple Application' and added into the website settings.py file

--- Back-End/SimpleWebsite/SimpleWebsite/settings.py
+++ Back-End/SimpleWebsite/SimpleWebsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SimpleApplication.apps.SimpleapplicationConfig'
 ]
 
 MIDDLEWARE = [
